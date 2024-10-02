a61cbcc5869547cc567ee9869841c56e7bf3cf88
Kelly <k_yee16@u.pacific.edu>
2024-10-01 19:32:17 -0700

    A timer needs a delay, action listener, and start method.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,16 @@
 import acm.graphics.*;
 import acm.program.*;
+import acm.util.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes = 0;
+	private Timer timer;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,7 +20,21 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		timer = new Timer(1000, this);
+		timer.setInitialDelay(3000);
+        timer.start(); 
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		 numTimes++; 
+	        myLabel.setLabel("times called? " + numTimes); 
+        myLabel.move(5, 0);
+        
+        if (numTimes >= 10) {
+            timer.stop(); // Stop the timer
+            return; // Exit the method
+        }
+    }
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
